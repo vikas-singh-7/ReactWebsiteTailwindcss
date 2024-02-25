727dca427381234511726d9c3093959f97235ad9
vikas-singh-7 <vs7587186@gmail.com>
2024-02-25 17:42:34 +0530

    Navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-zinc-900 text-white">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ProjectSteps.md
@@ -0,0 +1,11 @@
+## Project steps (Ochi Designs Website & Learning Tailwind css)
+
+<h1> Lets create a cool webSite with tailwind and react</h1>
+
+<ol> 
+<li>
+<h1>Navbar first component</h1>
+
+</li>
+
+</ol>
